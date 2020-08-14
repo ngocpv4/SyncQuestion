4e09f26e0c665a61cefada1ca4ccba12a0f6043c
ngoc phan <ngocpv@rikkeisoft.com>
2020-08-14 08:28:26 +0700

    delete link

--- QuestionAPI/Models/PageResult.cs
+++ QuestionAPI/Models/PageResult.cs
@@ -7,8 +7,8 @@ namespace QuestionAPI.Models
 {
     public class PageResult<T>
     {
-        public Link links { get; set; }
         public int total_items { get; set; }
+
         public int item_per_page { get; set; }
         public List<T> data { get; set; }
     }
